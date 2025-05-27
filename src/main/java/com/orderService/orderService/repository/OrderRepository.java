bc5699e8f660012be0e8eadf7cbaeeccd1583e97
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-05-27 10:50:35 +0200

    create order

---
+++ src/main/java/com/orderService/orderService/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.orderService.orderService.repository;
+
+import com.orderService.orderService.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
--- src/main/java/com/orderService/orderService/service/OrderItemService.java
+++ src/main/java/com/orderService/orderService/service/OrderItemService.java
@@ -5,13 +5,17 @@ import com.orderService.orderService.client.UserClient;
 import com.orderService.orderService.dto.OrderItemRequestDTO;
 import com.orderService.orderService.dto.Product.ProductDTO;
 import com.orderService.orderService.dto.ProductRequest;
-import com.orderService.orderService.dto.User.UserDTO;
+
 import com.orderService.orderService.exception.customException.OrderItemException;
+import com.orderService.orderService.model.Order;
 import com.orderService.orderService.model.OrderItem;
 import com.orderService.orderService.repository.OrderItemRespository;
+import com.orderService.orderService.repository.OrderRepository;
+import jakarta.persistence.JoinColumn;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,11 +24,14 @@ public class OrderItemService {
     private final ProductClient productClient;
     private final OrderItemRespository orderItemRespository;
     private final UserClient userClient;
+    private final OrderRepository  orderRepository;
 
-    public OrderItemService(ProductClient productClient, OrderItemRespository orderItemRespository,UserClient userClient) {
+    public OrderItemService(ProductClient productClient, OrderItemRespository orderItemRespository,
+                            UserClient userClient,OrderRepository orderRepository) {
         this.productClient = productClient;
         this.orderItemRespository = orderItemRespository;
         this.userClient = userClient;
+        this.orderRepository = orderRepository;
     }
 
     public ResponseEntity<?> creteOrderItem(OrderItemRequestDTO dto) {
@@ -51,19 +58,41 @@ public class OrderItemService {
             if (!subQuantityResponse.getStatusCode().is2xxSuccessful() || subQuantityResponse.getBody() == null) {
                 throw new OrderItemException("Not enough quantity for product with ID " + product.getProductId());
             }
+
             // create order item
             OrderItem orderItem = new OrderItem();
-            UserDTO user = (UserDTO) userResponse.getBody();
-            Long userId=userResponse.getBody().getId();
-            orderItem.setUserId(userId);
-
+            orderItem.setProductId(product.getProductId());
+            orderItem.setUserId(userResponse.getBody().getId());
+            orderItem.setQuantity(product.getQuantity());
+            orderItem.setPrice(productResponse.getBody().getPrice() * product.getQuantity());
+            orderItems.add(orderItem);
+            // save orderItems
+            orderItemRespository.save(orderItem);
         }
 
-
-
-
-
-
-
+        // create order
+        Order order = new Order();
+        Long userId=userResponse.getBody().getId();
+        order.setUserId(userId);
+        order.setOrderDate(LocalDateTime.now());
+        order.setOrderItems(orderItems);
+        // calculate total price
+        double totalPrice = calculateTotalPrice(orderItems);
+        order.setTotalAmount(totalPrice);
+        // save order item
+        Order savedOrder = orderRepository.save(order);
+        return ResponseEntity.ok().body(savedOrder);
     }
+
+
+    public double calculateTotalPrice(List<OrderItem> orderItems) {
+        double totalPrice = 0.0;
+        for (OrderItem item : orderItems) {
+            totalPrice+=item.getPrice()* item.getQuantity();
+        }
+        return totalPrice;
+    }
+
+
+
 }
